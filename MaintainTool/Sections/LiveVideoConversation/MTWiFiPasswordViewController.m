9d0d9f1204a5b9d50e68b1e8a90a98b75403e138
rongbh <channing.rong@gmail.com>
2019-01-11 10:17:41 +0800

    Changed the version number to V1.0.2.1.0111.

--- MaintainTool/Sections/LiveVideoConversation/MTWiFiPasswordViewController.m
+++ MaintainTool/Sections/LiveVideoConversation/MTWiFiPasswordViewController.m
@@ -42,18 +42,6 @@
 @implementation MTWiFiPasswordViewController
 
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-
-    
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
